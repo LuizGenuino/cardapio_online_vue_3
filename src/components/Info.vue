52b7c58344983f5df491bfb9e74ab044343777a6
genuinoLuiz <luizgustavo@pitom.com.br>
2025-04-02 15:19:38 -0400

    feat: introduction to v-if, v-show and v-for directives

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento</p>
+        <p v-if="isWorking" >Estou trabalhando no momento</p>
+        <p v-else >Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
-        <ul>
-            <li>Js</li>
-            <li>php</li>
-            <li>python</li>
+        <ul v-for="item in skills" :key="item">
+            <li>{{ item }}</li>
         </ul>
+        <p v-show="show_email" >Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 <script>
 export default {
-    name: "Info"
+    name: "Info",
+    data() {
+        return {
+            isWorking: true,
+            skills: ["JS", "PHP", "Python"],
+            show_email: false,
+            email: "luizgustavo@email.com"
+        }
+    }
 }
 </script>
