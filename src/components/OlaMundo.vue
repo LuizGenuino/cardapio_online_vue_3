3416c53613d0da224a6451a6220e3d5a677cea7f
genuinoLuiz <luizgustavo@pitom.com.br>
2025-04-02 14:03:32 -0400

    feat: introduction to vue 3 components and lifecycle

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Ola Vue</h1>
+        <p>Meu nome é {{ name }} e trabalho como {{ job }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "OlaMundo",
+    data() {
+        return {
+            name: "Luiz",
+            job: "Programador"
+        }
+    }
+}
+</script>
