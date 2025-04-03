7f0b1434f7a29606a8e8dc40844d2cb912378dff
genuinoLuiz <luizgustavo@pitom.com.br>
2025-04-03 13:01:18 -0400

    feat: introduction to methods in vue 3

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,23 +1,49 @@
 <template>
     <div>
-        <p v-if="isWorking" >Estou trabalhando no momento</p>
-        <p v-else >Estou em busca de novas oportunidades!</p>
+        <p v-if="isWorking">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul v-for="item in skills" :key="item">
             <li>{{ item }}</li>
         </ul>
-        <p v-show="show_email" >Mande uma mensagem para: {{ email }}</p>
+        <div>
+            <button @click="showEmail" >{{ textButton }}</button>
+        </div>
+        <p v-show="show_email">Mande uma mensagem para: {{ email }}</p>
+        <!-- v-bind: ou apenas : é a mesma coisa -->
+        <p>Para acessar meu portifolio <a v-bind:href="link" target="_blank">Clique Aqui</a></p>
+        <Picture></Picture>
     </div>
 </template>
 <script>
+
+import Picture from "./Picture.vue"
+
 export default {
     name: "Info",
+    components: {
+        Picture
+    },
     data() {
         return {
             isWorking: true,
             skills: ["JS", "PHP", "Python"],
             show_email: false,
-            email: "luizgustavo@email.com"
+            email: "luizgustavo@email.com",
+            link: "https://www.google.com",
+            textButton: "Mostrar email"
+        }
+    },
+
+    methods: {
+        showEmail(){
+            this.show_email = !this.show_email
+            if(this.show_email){
+                this.textButton = "Ocultar email" 
+            }
+            else{
+                this.textButton = "Mostrar email" 
+            }
         }
     }
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- v-bind: ou apenas : é a mesma coisa -->
+        <img :src="avatar" v-bind:alt="descption"> 
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Picture",
+        data() {
+            return {
+                avatar: "https://www.google.com/imgres?q=avatar%20png&imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F340%2F946%2Fpng-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fpt%2Fsearch%3Fq%3Davatar&docid=IzCvXibAmSsyhM&tbnid=KMpZDXFLoQCgjM&vet=12ahUKEwiC9bLIlrqMAxUcILkGHXJiB_sQM3oECG4QAA..i&w=360&h=395&hcb=2&ved=2ahUKEwiC9bLIlrqMAxUcILkGHXJiB_sQM3oECG4QAA",
+                descption: "Avatar",
+            }
+        }
+    }
+</script>
